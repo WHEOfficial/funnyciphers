d3cb635a4119fcb4e26cda64aa5a9f0589f877cd
WHEOfficial <46579241+WHEOfficial@users.noreply.github.com>
2022-10-12 09:08:52 -0400

    Added chi_squared to cipher.py

--- cipher.py
+++ cipher.py
@@ -6,6 +6,54 @@ NUMBER_MIN = 48
 NUMBER_MAX = 57
 NUMBER_RANGE = range(NUMBER_MIN, NUMBER_MAX + 1)
 
+FREQUENCIES = {
+    'E': 0.1249,
+    'T': 0.0928,
+    'A': 0.0804,
+    'O': 0.0764,
+    'I': 0.0757,
+    'N': 0.0723,
+    'S': 0.0651,
+    'R': 0.0628,
+    'H': 0.0505,
+    'L': 0.0407,
+    'D': 0.0382,
+    'C': 0.0334,
+    'U': 0.0273,
+    'M': 0.0251,
+    'F': 0.024,
+    'P': 0.0214,
+    'G': 0.0187,
+    'W': 0.0168,
+    'Y': 0.0166,
+    'B': 0.0148,
+    'V': 0.0105,
+    'K': 0.0054,
+    'X': 0.0023,
+    'J': 0.0016,
+    'Q': 0.0012,
+    'Z': 0.0009,
+}
+
+def chi_squared(text):
+    text_freqs = {}
+
+    chi_squared_value = 0
+    length = 0
+
+    for c in text.upper():
+        if ord(c) in LETTER_RANGE:
+            text_freqs[c] = text_freqs.get(c, 0) + 1
+            length += 1
+    
+    for k, v in FREQUENCIES.items():
+        actual = text_freqs.get(k, 0)
+        expected = v * length
+        diff = actual - expected
+        chi_squared_value += (diff*diff) / expected
+    
+    return chi_squared_value
+
 def caesar_encrypt(text, shift=3):
     text = text.upper()
     encrpyted = ""
